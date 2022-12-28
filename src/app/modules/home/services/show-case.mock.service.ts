908ea4a5a8850c812b73348fca839e1ef14f5ec9
Christian Galvez <christian.galvez@unbounce.com>
2022-12-28 01:09:34 -0700

    UI changes

--- src/app/modules/home/services/show-case.mock.service.ts
+++ src/app/modules/home/services/show-case.mock.service.ts
@@ -14,91 +14,91 @@ export class ShowCaseMockService implements IProductService {
       "id": 1,
       "name": "Product 1",
       "price": 9.99,
-      "image": "https://via.placeholder.com/150"
+      "image": "https://via.placeholder.com/160x180"
     },
     {
       "id": 2,
       "name": "Product 2",
       "price": 4.99,
-      "image": "https://via.placeholder.com/150"
+      "image": "https://via.placeholder.com/160x180"
     },
     {
       "id": 3,
       "name": "Product 3",
       "price": 3.99,
-      "image": "https://via.placeholder.com/150"
+      "image": "https://via.placeholder.com/160x180"
     },
     {
       "id": 4,
       "name": "Product 4",
       "price": 4.99,
-      "image": "https://via.placeholder.com/150"
+      "image": "https://via.placeholder.com/160x180"
     },
     {
       "id": 5,
       "name": "Product 5",
       "price": 2.99,
-      "image": "https://via.placeholder.com/150"
+      "image": "https://via.placeholder.com/160x180"
     },
     {
       "id": 6,
       "name": "Product 6",
       "price": 3.99,
-      "image": "https://via.placeholder.com/150"
+      "image": "https://via.placeholder.com/160x180"
     },
     {
       "id": 7,
       "name": "Product 7",
       "price": 7.99,
-      "image": "https://via.placeholder.com/150"
+      "image": "https://via.placeholder.com/160x180"
     },
     {
       "id": 8,
       "name": "Product 8",
       "price": 6.99,
-      "image": "https://via.placeholder.com/150"
+      "image": "https://via.placeholder.com/160x180"
     },
     {
       "id": 9,
       "name": "Product 9",
       "price": 8.99,
-      "image": "https://via.placeholder.com/150"
+      "image": "https://via.placeholder.com/160x180"
     },
     {
       "id": 10,
       "name": "Product 10",
       "price": 5.99,
-      "image": "https://via.placeholder.com/150"
+      "image": "https://via.placeholder.com/160x180"
     },
     {
       "id": 11,
       "name": "Product 11",
       "price": 1.99,
-      "image": "https://via.placeholder.com/150"
+      "image": "https://via.placeholder.com/160x180"
     },
     {
       "id": 12,
       "name": "Product 12",
       "price": 2.99,
-      "image": "https://via.placeholder.com/150"
+      "image": "https://via.placeholder.com/160x180"
     },
     {
       "id": 13,
       "name": "Product 13",
       "price": 3.99,
-      "image": "https://via.placeholder.com/150"
+      "image": "https://via.placeholder.com/160x180"
     },
     {
       "id": 14,
       "name": "Product 14",
       "price": 2.99,
-      "image": "https://via.placeholder.com/150"
+      "image": "https://via.placeholder.com/160x180"
     },
     {
       "id": 15,
       "name": "Product 15",
       "price": 99.99,
-      "image": "https://via.placeholder.com/150"
+      "image": "https://via.placeholder.com/160x180"
     }
   ];
 
--- src/app/modules/shared/components/product-info/product-info.component.ts
+++ src/app/modules/shared/components/product-info/product-info.component.ts
@@ -3,11 +3,12 @@ import { Product } from "../../../core/models/product.interface";
 import { Store } from "@ngrx/store";
 import { AppState } from "../../../store/app.state";
 import { enableDisableProduct } from "../../../store/actions/products.actions";
+import { formatCurrency } from "@angular/common";
 
 export enum ProductInfoView {
   ShowCase,
   Basket
-};
+}
 
 @Component({
   selector: 'app-product-info',
@@ -19,6 +20,12 @@ export class ProductInfoComponent {
   @Input() product!: Product;
 
   public productInfoView = ProductInfoView;
+
+  get buttonText(): string {
+    return this.product?.inBasket
+      ? 'REMOVE FROM BASKET'
+      : `ADD TO BASKET ${formatCurrency(this.product.price, 'en-US', '$', '1.1-2')}`;
+  }
 
   constructor(public readonly store: Store<AppState>) {
     this.view = ProductInfoView.ShowCase;
